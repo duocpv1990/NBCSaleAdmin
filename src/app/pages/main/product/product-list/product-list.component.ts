7f9629def4ac147eda63416297df48a7d3c1575f
Duoc <duocpv@consultindochina.com>
2021-05-10 17:38:40 +0700

    +add filter

--- src/app/pages/main/product/product-list/product-list.component.ts
+++ src/app/pages/main/product/product-list/product-list.component.ts
@@ -6,6 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent implements OnInit {
+  dataFilterCard = [
+    {
+      type: {
+        Id: 'id',
+        Phone: 'phone',
+        startDate: 'startDate',
+        endDate: 'endDate',
+        Status: 'status',
+      },
+    },
+  ];
 
   constructor() { }
 
--- src/app/pages/main/product/product.module.ts
+++ src/app/pages/main/product/product.module.ts
@@ -5,13 +5,16 @@ import { RouterModule } from '@angular/router';
 import { productRoute } from './product.routes';
 import { ProductComponent } from './product.component';
 import { ProductListComponent } from './product-list/product-list.component';
-
+import { BaseCardModule } from 'src/app/components/card/base-card/base-card.component';
+import { FilterCardModule } from 'src/app/components/card/filter-card/filter-card.component';
 
 @NgModule({
   declarations: [ProductComponent, ProductListComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(productRoute),
+    BaseCardModule,
+    FilterCardModule
   ],
   exports: [ProductComponent]
 })
